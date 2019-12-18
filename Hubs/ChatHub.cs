3787d23278f6e7cf93f098b88607bb20449e24b3
Ediz Ilkcakin <ediz.ilkcakin@ukwebservice.com>
2019-12-18 10:44:11 +0300

    SignalR with IdentityServer 4

---
+++ Hubs/ChatHub.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using SignalRDemo.Models;
+using System.Threading.Tasks;
+
+namespace SignalRDemo.Hubs
+{
+    public class ChatHub : Hub
+    {
+        public async Task SendMessage(Message message) =>
+            
+            await Clients.All.SendAsync("receiveMessage", message);
+    }
+}
